5963bbb0842121c9aab2a57ad4e358f6be07a6f5
Joel Nises <joel.nises@gmail.com>
2023-07-18 20:48:16 +0200

    compress

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use std::io::{Write, Read};
+use std::io::{Read, Write};
 
-use termcolor::{StandardStream, WriteColor, ColorChoice, Color, ColorSpec};
+use flate2::{write::DeflateEncoder, Compression};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() -> anyhow::Result<()> {
     let stdin = std::io::stdin();
     let mut si = stdin.lock();
     let stdout = StandardStream::stdout(ColorChoice::Always);
-    //let stdout = std::io::stdout();
     let mut so = stdout.lock();
+    let mut e = DeflateEncoder::new(vec![], Compression::default());
+    let mut text = vec![];
     loop {
         let mut buf = vec![0u8; 1024];
         let size = si.read(&mut buf)?;
         let data = &buf[..size];
-        so.set_color(ColorSpec::new().set_fg(Some(color_map(0.5))))?;
-        so.write_all(&data)?;
+        e.write_all(data)?;
+        e.flush()?;
+        text.extend(data.iter());
+        let v = e.get_mut();
+        let len = v.len();
+        // TODO: does this mean that `data` was written, or just that the buffer before `data` was written?
+        if len > 0 {
+            v.clear();
+            let probability = (len as f32 / text.len() as f32).clamp(0.0, 1.0);
+            so.set_color(ColorSpec::new().set_fg(Some(color_map(probability))))?;
+            so.write_all(&text)?;
+            text.clear();
+        }
     }
 }
 
@@ -21,4 +34,3 @@ fn color_map(t: f32) -> Color {
     let [r, g, b, _] = colorgrad::spectral().at(t.into()).to_rgba8();
     Color::Rgb(r, g, b)
 }
-
