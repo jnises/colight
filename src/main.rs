d7ec0186deab37efa09f603644115bd959cb633f
Joel Nises <joel.nises@gmail.com>
2023-07-21 14:37:48 +0200

    compression per line

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn main() -> anyhow::Result<()> {
     let mut so = stdout.lock();
     let mut e = DeflateEncoder::new(vec![], Compression::default());
     let mut line = String::new();
+    let mut prev_in = 0;
+    let mut prev_out = 0;
     loop {
         line.clear();
         if si.read_line(&mut line)? == 0 {
@@ -17,8 +19,11 @@ fn main() -> anyhow::Result<()> {
         }
         e.write_all(line.as_bytes())?;
         e.flush()?;
-        let compression = e.total_out() as f32 / e.total_in() as f32;
-        // TODO: test without this line
+        let line_in = e.total_in() - prev_in;
+        let line_out = e.total_out() - prev_out;
+        prev_in = e.total_in();
+        prev_out = e.total_out();
+        let compression = line_out as f32 / line_in as f32;
         e.get_mut().clear();
         so.set_color(ColorSpec::new().set_fg(Some(color_map(compression))))?;
         so.write_all(line.as_bytes())?;
